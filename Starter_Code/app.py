4f6dd53bfcb621bf9265ebebc5ad0f69ebf6383b
Geysha07 <132965919+Geysha07@users.noreply.github.com>
2023-12-04 12:45:06 -0500

    Added more code to Flask routes

--- Starter_Code/app.py
+++ Starter_Code/app.py
@@ -26,7 +26,7 @@ measurement = Base.classes.measurement
 station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-session = Session(engine)
+
 
 #################################################
 # Flask Setup
@@ -39,15 +39,48 @@ app = Flask(__name__)
 # Flask Routes
 #################################################
 @app.route("/")
-
+def welcome():
+    """List all available api routes."""
+    return(
+        f"Welcome to Hawaii Weather Data API"
+        f"/api/v1.0/precipitation"
+        f"/api/v1.0/stations"
+        f"/api/v1.0/tobs"
+        f"/api/v1.0/<start>"
+    )
 
 @app.route("/api/v1.0/precipitation")
+def precipitation():
 
+    session = Session(engine)
+    
+    precipitation_results = session.query(measurement.prcp, measurement.date).all()
+    
+    session.close()
+
+    precipitation_data = []
+    for prcp, date in precipitation_results:
+        precipitation_dict = []
+        precipitation_dict ["precipitation"] = prcp
+        precipitation_dict ["date"] = date
+        precipitation_data.append(precipitation_dict)
+    return jsonify(precipitation_data)
 
 
 @app.route("/api/v1.0/stations")
+def station():
+    
+    session = Session(engine)
+    station_results = session.query(station.station).all()
 
+    session.close()
 
+    station_list = []
+    for station in station_results:
+        stations_list_dict = []
+        stations_list_dict['station'] = station
+        station_list.append(stations_list_dict)
+    return jsonify(station_list)
 
 @app.route("/api/v1.0/tobs")
 
